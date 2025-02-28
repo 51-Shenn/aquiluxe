ffdcd9b6c52d2c3c32e2db363894c132d3e8d245
briankdx <briankdx-wm24@student.tarc.edu.my>
2025-02-28 16:52:37 +0800

    add address variable

--- Assignment/src/datamodels/Customer.java
+++ Assignment/src/datamodels/Customer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer extends User {
+    private String address;
     private List<Rental> rentalHistory;
 
     // Default Constructor
@@ -14,17 +15,26 @@ public class Customer extends User {
 
     // Parameter Constructor
     public Customer(int userId, String fullName, String gender, String license, String phoneNumber, String userEmail,
-            String username, String password) {
+            String address, String username, String password) {
         super(userId, fullName, gender, license, phoneNumber, userEmail, username, password);
+        this.address = address;
         this.rentalHistory = new ArrayList<>(); // No need pass to User class
     }
 
     // Getters and Setters
+    public String getUserAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public List<Rental> getRentalHistory() {
         return rentalHistory;
     }
 
-    // Add Rental to History Method
+    // Add Rental to History
     public void addRental(Rental rental) {
         this.rentalHistory.add(rental);
     }
