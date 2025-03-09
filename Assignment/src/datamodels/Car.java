b8a598555acdd00288d429026b006001b493010e
season1ng <168104607+season1ng@users.noreply.github.com>
2025-03-09 21:16:08 +0800

    add car and bike subclass and add other minor gui stuff

---
+++ Assignment/src/datamodels/Car.java
@@ -0,0 +1,17 @@
+package datamodels;
+
+import java.util.List;
+
+public class Car extends Vehicle {
+    // Default Constructor
+    public Car(){
+        super();
+    }
+
+    // Parameterized Constructor
+    public Car(int vehicleId, String imagePath, String brand, String model, int year, int capacity,
+            int horsepower, String color, double mpg, String vinNumber, String registrationNumber,
+            double rentalPriceDay, String transmission, String fuelType, int seatingCapacity, boolean availability, List<String> features) {
+        super(vehicleId,imagePath,brand,model,year,capacity,horsepower,color,mpg,vinNumber,registrationNumber,rentalPriceDay,transmission,fuelType,seatingCapacity,availability,features);
+    }
+}
