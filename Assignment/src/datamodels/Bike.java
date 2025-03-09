3e12a2f40ecad2310cbe4b6a5847c300788d10c9
season1ng <168104607+season1ng@users.noreply.github.com>
2025-03-09 21:40:42 +0800

    minor fix for subclass

--- Assignment/src/datamodels/Bike.java
+++ Assignment/src/datamodels/Bike.java
@@ -11,7 +11,8 @@ public class Bike extends Vehicle{
     // Parameterized Constructor
     public Bike(int vehicleId, String imagePath, String brand, String model, int year, int capacity,
             int horsepower, String color, double mpg, String vinNumber, String registrationNumber,
-            double rentalPriceDay, String transmission, String fuelType, int seatingCapacity, boolean availability, List<String> features) {
-        super(vehicleId,imagePath,brand,model,year,capacity,horsepower,color,mpg,vinNumber,registrationNumber,rentalPriceDay,transmission,fuelType,seatingCapacity,availability,features);
+            double rentalPriceDay, String transmission, String fuelType, String carType, int seatingCapacity, boolean availability, List<String> features) {
+        super(vehicleId,imagePath,brand,model,year,capacity,horsepower,color,mpg,vinNumber,registrationNumber,rentalPriceDay,transmission,fuelType,carType,seatingCapacity,availability,features);
+        carType = null;
     }
 }
