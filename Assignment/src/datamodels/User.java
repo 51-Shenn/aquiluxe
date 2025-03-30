833ef4c162b6dbb15fc62eb66c44de19bdb0d5fc
briankdx <briankdx-wm24@student.tarc.edu.my>
2025-03-31 01:44:24 +0800

    fix user data model error

--- Assignment/src/datamodels/User.java
+++ Assignment/src/datamodels/User.java
@@ -34,19 +34,21 @@ public class User {
     }
 
     public User(String fullName, String gender, String phoneNumber, String userEmail, String username,
-            String password) {
+            String password, String usertype) {
         this.fullName = fullName;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
         this.username = username;
         this.userEmail = userEmail;
         this.password = password;
+        this.userType = usertype;
 
         users.put(userId, this);
     }
 
     public User(int userId, String fullName, String gender, String phoneNumber, String userEmail, String username,
             String password, String userType) {
+        this.userId = userId;
         this.fullName = fullName;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
@@ -54,8 +56,6 @@ public class User {
         this.userEmail = userEmail;
         this.password = password;
         this.userType = userType;
-
-        users.put(userId, this);
     }
 
     // Getters and Setters
