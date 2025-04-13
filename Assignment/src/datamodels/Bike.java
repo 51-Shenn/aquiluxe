95e7c43f4aa3af63b1c427694950bab48a8e90fa
season1ng <168104607+season1ng@users.noreply.github.com>
2025-04-14 01:23:48 +0800

    change datamodels for bike and car

--- Assignment/src/datamodels/Bike.java
+++ Assignment/src/datamodels/Bike.java
@@ -12,11 +12,11 @@ public class Bike extends Vehicle {
     // Parameterized Constructor
     public Bike(int vehicleId, String imagePath, String brand, String model, int year, int capacity,
             int horsepower, String color, double mpg, String vinNumber, String registrationNumber,
-            double rentalPriceDay, String transmission, String fuelType, String bikeType, int seatingCapacity,
+            double rentalPriceDay, String transmission, String fuelType, String vehicleType, int seatingCapacity,
             boolean availability, String features) {
         super(vehicleId, imagePath, brand, model, year, capacity, horsepower, color, mpg, vinNumber, registrationNumber,
                 rentalPriceDay, transmission, fuelType, seatingCapacity, availability, features);
-        this.bikeType = bikeType;
+        this.bikeType = vehicleType;
     }
 
     // Getter and Setters
