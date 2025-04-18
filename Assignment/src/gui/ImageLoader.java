52b310a235c39105767ca5954a46b084e0cb9769
Shenn <yshen0505@gmail.com>
2025-04-18 19:27:54 +0800

    added file checking for ImageLoader.java

--- Assignment/src/gui/ImageLoader.java
+++ Assignment/src/gui/ImageLoader.java
@@ -3,35 +3,54 @@ package gui;
 import java.awt.Image;
 import java.io.File;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class ImageLoader {
 
-    private static final File whitePorsche;
-    private static final File signInWallpaper;
-    private static final File signUpWallpaper;
-    private static final File forgotPasswordWallpaper;
+    private static final File WHITE_PORSCHE;
+    private static final File SIGN_IN_WALLPAPER;
+    private static final File SIGN_UP_WALLPAPER;
+    private static final File FORGOT_PASSWORD_WALLPAPER;
 
     static {
-        whitePorsche = new File("images/wallpapers/porsche-white.png");
-        signInWallpaper = new File("images/wallpapers/car-wallpaper-3.png");
-        signUpWallpaper = new File("images/wallpapers/car-wallpaper-2.png");
-        forgotPasswordWallpaper = new File("images/wallpapers/car-wallpaper-1.png");
+        WHITE_PORSCHE = new File("images/wallpapers/porsche-white.png");
+        SIGN_IN_WALLPAPER = new File("images/wallpapers/car-wallpaper-3.png");
+        SIGN_UP_WALLPAPER = new File("images/wallpapers/car-wallpaper-2.png");
+        FORGOT_PASSWORD_WALLPAPER = new File("images/wallpapers/car-wallpaper-1.png");
+
+        File[] images = {
+            WHITE_PORSCHE,
+            SIGN_IN_WALLPAPER,
+            SIGN_UP_WALLPAPER,
+            FORGOT_PASSWORD_WALLPAPER
+        };
+
+        checkEachImageExistence(images);
+    }
+
+    private static void checkEachImageExistence(File[] images) {
+        for(File image : images) {
+            if (!image.exists()) {
+                JOptionPane.showMessageDialog(null, "Failed to load image:\n" + image + "\n");
+                break;
+            }
+        }
     }
 
     public static ImageIcon getWhitePorscheImage() {
-        return scaleImage(whitePorsche, 1100, 609);
+        return scaleImage(WHITE_PORSCHE, 1100, 609);
     }
 
     public static ImageIcon getSignInWallpaper() {
-        return scaleImage(signInWallpaper, 960, 1080);
+        return scaleImage(SIGN_IN_WALLPAPER, 960, 1080);
     }
 
     public static ImageIcon getSignUpWallpaper() {
-        return scaleImage(signUpWallpaper, 960, 1080);
+        return scaleImage(SIGN_UP_WALLPAPER, 960, 1080);
     }
 
     public static ImageIcon getForgotPasswordWallpaper() {
-        return scaleImage(forgotPasswordWallpaper, 960, 1080);
+        return scaleImage(FORGOT_PASSWORD_WALLPAPER, 960, 1080);
     }
 
     private static ImageIcon scaleImage(File imageFile, int width, int height) {
